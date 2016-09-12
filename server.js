3b0c35358626a974cee563d10f1966771f26d46c
lgargantini <leandrofgargantini@gmail.com>
2016-09-12 09:22:05 -0300

    first draft http2

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ var io = require('socket.io')(server);
 var session = require('express-session');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
+var spdy = require('http2');
+var fs = require('fs');
+var options = {
+	key:fs.readFileSync(__dirname + '/node-http/server.key'),
+	cert:fs.readFileSync(__dirname + '/node-http/server.crt')
+};
 
 var port = Number(process.env.PORT || 8000);
 var positions = {};
@@ -26,6 +32,10 @@ app.use(session({secret: '123456789QWERTY',
 var control = require('./controllers/index.js')(positions, messages, files, latencies);
 var routes = require('./routes')(app,control,io,positions, messages,files, latencies);
 
+require('http2').createServer(options, function(request, response) {
+  response.end('Hello world!');
+}).listen(5000);
+/*
 server.listen(port,function () {
     console.log('listening on -> '+port);
-});
+});*/
