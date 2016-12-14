6dec9d42e17bd5097bf4bfe42fe0fc0993865456
Leandro Gargantini <leandrofgargantini@gmail.com>
2016-12-14 15:24:25 -0300

    http2 - with all test

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 'use strict';
 var express = require('express');
 var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-var session = require('express-session');
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
-var spdy = require('http2');
 var fs = require('fs');
 var options = {
 	key:fs.readFileSync(__dirname + '/node-http/server.key'),
 	cert:fs.readFileSync(__dirname + '/node-http/server.crt')
 };
+var server = require('http').Server(app);
+var spdy = require('spdy').createServer(options, app);
+var io = require('socket.io')(server);
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 
 var port = Number(process.env.PORT || 8000);
+var port2 = Number(process.env.PORT2 || 5000);
 var positions = {};
 var messages = {};
 var files = {};
@@ -32,10 +33,11 @@ app.use(session({secret: '123456789QWERTY',
 var control = require('./controllers/index.js')(positions, messages, files, latencies);
 var routes = require('./routes')(app,control,io,positions, messages,files, latencies);
 
-require('http2').createServer(options, function(request, response) {
-  response.end('Hello world!');
-}).listen(5000);
-/*
+spdy
+.listen(port2, function () {
+	console.log('http/2 listening on ->'+port2)
+});
+
 server.listen(port,function () {
-    console.log('listening on -> '+port);
-});*/
+    console.log('http/1.x - ws listening on -> '+port);
+});
