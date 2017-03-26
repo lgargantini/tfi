b2d7ef6351e7101797f203d6152e9e3c4120ded6
Leandro Gargantini <leandrofgargantini@gmail.com>
2017-03-26 03:06:12 -0300

    adjust index

--- controllers/general.js
+++ controllers/general.js
@@ -3,20 +3,6 @@ module.exports = function (positions) {
 var general = {
 
 	index:function(req,res) {
-		if(req.secure){
-			var stream = res.push('httpClientHandler.js',{
-		    	status:200,
-		    	method:'GET',
-		    	request:{
-		    		accept:'*/*'
-		    	},
-		    	response:{
-		    		'content-type':'application/javascript'
-		    	}
-		    });
-		    stream.end('alert("hello from push stream!");');
-	    }
-	    console.log('/ get');
 	    res.render('index');
 	},
 	clear:function  (req,res) {
