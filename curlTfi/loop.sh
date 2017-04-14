1009c1803f9b3618e27f240fe9af6b82abacdb62
Leandro Gargantini <leandrofgargantini@gmail.com>
2017-04-14 19:56:42 -0300

    adjust loop script

--- curlTfi/loop.sh
+++ curlTfi/loop.sh
@@ -1,31 +1,37 @@
 echo "setting headers"
-echo "size_header size_request time_namelookup time_connect time_appconnect time_pretransfer time_redirect time_starttransfer time_total" > resHttp10.csv
-echo "size_header size_request time_namelookup time_connect time_appconnect time_pretransfer time_redirect time_starttransfer time_total" > resHttps10.csv
-echo "size_header size_request time_namelookup time_connect time_appconnect time_pretransfer time_redirect time_starttransfer time_total" > resHttp100.csv
-echo "size_header size_request time_namelookup time_connect time_appconnect time_pretransfer time_redirect time_starttransfer time_total" > resHttps100.csv
-echo "size_header size_request time_namelookup time_connect time_appconnect time_pretransfer time_redirect time_starttransfer time_total" > resHttp20.csv
-echo "size_header size_request time_namelookup time_connect time_appconnect time_pretransfer time_redirect time_starttransfer time_total" > resHttps20.csv
+echo "size_header size_request time_namelookup time_connect time_appconnect time_pretransfer time_redirect time_starttransfer time_total" > resH1_10.csv
+echo "size_header size_request time_namelookup time_connect time_appconnect time_pretransfer time_redirect time_starttransfer time_total" > resH1s_10.csv
+echo "size_header size_request time_namelookup time_connect time_appconnect time_pretransfer time_redirect time_starttransfer time_total" > resH2_10.csv
+echo "size_header size_request time_namelookup time_connect time_appconnect time_pretransfer time_redirect time_starttransfer time_total" > resH1_20.csv
+echo "size_header size_request time_namelookup time_connect time_appconnect time_pretransfer time_redirect time_starttransfer time_total" > resH1s_20.csv
+echo "size_header size_request time_namelookup time_connect time_appconnect time_pretransfer time_redirect time_starttransfer time_total" > resH2_20.csv
+echo "size_header size_request time_namelookup time_connect time_appconnect time_pretransfer time_redirect time_starttransfer time_total" > resH1_100.csv
+echo "size_header size_request time_namelookup time_connect time_appconnect time_pretransfer time_redirect time_starttransfer time_total" > resH1s_100.csv
+echo "size_header size_request time_namelookup time_connect time_appconnect time_pretransfer time_redirect time_starttransfer time_total" > resH2s_100.csv
 echo "done!"
 
 echo "curl loop 10 ..."
 for ((i=1;i<=10;i++)); 
 do
-curl -w "@curl-format.txt" -o /dev/null -s https://tfium.eastus.cloudapp.azure.com:8000 >> resHttp10.csv;
-curl -w "@curl-format.txt" -o /dev/null -s https://tfium.eastus.cloudapp.azure.com >> resHttps10.csv;
+curl -w "@curl-format.txt" -o /dev/null -s http://tfium.eastus.cloudapp.azure.com:5000 >> resH1_10.csv;
+curl -w "@curl-format.txt" -o /dev/null -s https://tfium.eastus.cloudapp.azure.com:8000 >> resH1s_10.csv;
+curl -w "@curl-format.txt" -o /dev/null -s https://tfium.eastus.cloudapp.azure.com >> resH2_10.csv;
 done
 
 echo "curl loop 20 ..."
 for ((i=1;i<=20;i++)); 
 do 
-curl -w "@curl-format.txt" -o /dev/null -s https://tfium.eastus.cloudapp.azure.com:8000 >> resHttp20.csv;
-curl -w "@curl-format.txt" -o /dev/null -s https://tfium.eastus.cloudapp.azure.com >> resHttps20.csv;
+curl -w "@curl-format.txt" -o /dev/null -s http://tfium.eastus.cloudapp.azure.com:5000 >> resH1_20.csv;
+curl -w "@curl-format.txt" -o /dev/null -s https://tfium.eastus.cloudapp.azure.com:8000 >> resH1s_20.csv;
+curl -w "@curl-format.txt" -o /dev/null -s https://tfium.eastus.cloudapp.azure.com >> resH2_20.csv;
 done
 
 echo "curl loop 100 ..."
 for ((i=1;i<=100;i++)); 
 do
-curl -w "@curl-format.txt" -o /dev/null -s https://tfium.eastus.cloudapp.azure.com:8000 >> resHttp100.csv;
-curl -w "@curl-format.txt" -o /dev/null -s https://tfium.eastus.cloudapp.azure.com >> resHttps100.csv;
+curl -w "@curl-format.txt" -o /dev/null -s https://tfium.eastus.cloudapp.azure.com:5000 >> resH1_100.csv;
+curl -w "@curl-format.txt" -o /dev/null -s https://tfium.eastus.cloudapp.azure.com:8000 >> resH1s_100.csv;
+curl -w "@curl-format.txt" -o /dev/null -s https://tfium.eastus.cloudapp.azure.com >> resH2_100.csv;
 done
 
 echo "done -> bye!"
